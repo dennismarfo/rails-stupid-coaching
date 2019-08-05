33a0e88c01dcb5376c5b43f10677f247e2fc8e8a
Denis Kojo <dennismarfo@gmail.com>
2019-08-05 15:39:54 +0200

    "update"

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+    @answer = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      return @answer = 'Great!'
+    elsif @question.end_with?('?')
+      return @answer = 'Silly question, get dressed and go to work!'
+    else
+      return @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
